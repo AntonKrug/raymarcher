4709eb23f8b7e19c5ee80bb4cf42179476e02d91
Anton Krug <anton.krug@gmail.com>
2021-12-26 23:20:38 +0000

    not static

--- src/helper.cpp
+++ src/helper.cpp
@@ -6,17 +6,17 @@
 
 namespace helper {
 
-  float static fminfast2(float a, float b) {
+  float fminfast2(float a, float b) {
     return a > b ? b : a;
   }
 
 
-  float static fmaxfast2(float a, float b) {
+  float fmaxfast2(float a, float b) {
     return a > b ? a : b;
   }
 
 
-  float static clamp(float a, float min, float max) {
+  float clamp(float a, float min, float max) {
     return fminfast2(fmaxfast2(a, min), max);
   }
 
--- src/helper.h
+++ src/helper.h
@@ -9,9 +9,9 @@
 namespace helper {
 
   // For RISC-V baremetal + newlib target this has to be written in asm
-  float static fminfast2(float a, float b);
-  float static fmaxfast2(float a, float b);
-  float static clamp(float a, float min, float max);
+  float fminfast2(float a, float b);
+  float fmaxfast2(float a, float b);
+  float clamp(float a, float min, float max);
 
 };
 
