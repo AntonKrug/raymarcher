ce1847d15367b7d70a60d90744d7c538ae1a0373
Anton Krug <anton.krug@gmail.com>
2021-12-27 17:42:03 +0000

    Adding min/max for 3 arguments

--- src/helper.cpp
+++ src/helper.cpp
@@ -10,11 +10,17 @@ namespace helper {
     return (a > b) ? b : a;
   }
 
+  float fminfast3(float a, float b, float c) {
+    return fminfast2(fminfast2(a, b), c);
+  }
 
   float fmaxfast2(float a, float b) {
     return (a > b) ? a : b;
   }
 
+  float fmaxfast3(float a, float b, float c) {
+    return fmaxfast2(fmaxfast2(a, b), c);
+  }
 
   float clamp(float a, float min, float max) {
     return fminfast2(fmaxfast2(a, min), max);
