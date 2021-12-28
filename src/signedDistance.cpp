fd3f64e60b7e4b2e7b63b4720aa504357d26fdb6
Anton Krug <anton.krug@gmail.com>
2021-12-28 13:30:54 +0000

    Box distance, not just to the edge diagonal, but to the corners too

--- src/signedDistance.cpp
+++ src/signedDistance.cpp
@@ -11,8 +11,7 @@ namespace signedDistance {
 
   float box(vector currentPoint, vector size) {
     vector distance = currentPoint.abs() - size;
-    distance.maxInPlace(0.0f);
-    return distance.length();
+    return distance.max(0.0f).length() + helper::fminfast2(0.0f, helper::fmaxfast3(distance.x, distance.y, distance.z));
   }
 
 
@@ -26,13 +25,13 @@ namespace signedDistance {
     return helper::fmaxfast3(distance, -currentPoint.z - 0.25, currentPoint.z);
   }
 
-  
+
   float mhcpDodlyDood(vector currentPoint, vector base) {
 //    currentPoint = currentPoint - vector(currentPoint.y * -0.637f, currentPoint.x * 0.81714f, 0.0f);
     float roundNess = 0.25f;
-    float b1  = (box(currentPoint, vector(base.x-roundNess, base.y-roundNess, base.z))-roundNess);
-    float ba1 = (box(currentPoint -vector(+base.x/2.0f, -base.y/2.0f, 0.0f) , base/2.0f));
-    float bb1 = (box(currentPoint -vector(-base.x/2.0f, +base.y/2.0f, 0.0f), base/2.0f));
+    float b1  = box(currentPoint, vector(base.x-roundNess, base.y-roundNess, base.z))-roundNess;
+    float ba1 = box(currentPoint -vector(+base.x/2.0f, -base.y/2.0f, 0.0f) , base/2.0f);
+    float bb1 = box(currentPoint -vector(-base.x/2.0f, +base.y/2.0f, 0.0f), base/2.0f);
 
     return helper::fminfast3(b1,ba1,bb1);
   }
