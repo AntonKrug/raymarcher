136f898c634ef71c46fb07bec49bce02415b07fe
Anton Krug <anton.krug@gmail.com>
2021-12-28 16:09:26 +0000

    Simplify the cylinder calculations

--- src/signedDistance.cpp
+++ src/signedDistance.cpp
@@ -18,13 +18,9 @@ namespace signedDistance {
 
 
   float mhcpLogoCylinder(vector currentPoint) {
-    const vector thick = vector(0.0f, 0.0f, -0.25f);
-    const vector thickNormalized = vector(thick).normalize();
-
-    vector closePoint = thickNormalized.multiplyConst(currentPoint.dotProduct(thick));
-    float  distance   = (currentPoint - closePoint).length() - 1.8f;
-
-    return helper::fmaxfast3(distance, -currentPoint.z - 0.25, currentPoint.z);
+    currentPoint.z += helper::clamp(currentPoint.z, 0.0f, -0.25f);        // hardcoded line segment distance
+    float distance = currentPoint.length() - 1.8f;                                  // make it into capsule by using the sphere calculation
+    return helper::fmaxfast3(distance, -currentPoint.z - 0.25, currentPoint.z);  // cap both ends with planes to make it cylinder with flat ends
   }
 
 
