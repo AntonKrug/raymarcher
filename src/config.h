fbecd559ae9ccc3b070b7802238eb6cc8ed7ce7f
Anton Krug <anton.krug@gmail.com>
2021-12-28 18:18:32 +0000

    Slowdown the calculation to make the benchmarks more visible

--- src/config.h
+++ src/config.h
@@ -8,12 +8,12 @@
 
 namespace config {
   // Render resolution
-  constexpr int width  = 256;
-  constexpr int height = 240;
+  constexpr int width  = 256*2;
+  constexpr int height = 240*2;
 
   // Display resolution can bigger than render resolution (scaling the image)
-  constexpr int displayWidth  = width  * 4;
-  constexpr int displayHeight = height * 4;
+  constexpr int displayWidth  = width  * 2;
+  constexpr int displayHeight = height * 2;
 
   // Normal calculation offset
   constexpr float nudgeOffset = 0.01f;
