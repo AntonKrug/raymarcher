d82cc4278e8f282ec34e64cc64e9b25d22c333af
Anton Krug <anton.krug@gmail.com>
2021-12-29 17:09:35 +0000

    Using the scene directly

--- src/rayMarcher.h
+++ src/rayMarcher.h
@@ -8,14 +8,12 @@
 
 #include "tuple"
 
-//#include "scenes/all.h"
 #include "vector.h"
 #include "helper.h"
 #include "color.h"
 #include "outputSdl.h"
 #include "sampler.h"
 
-//const static std::array<std::tuple<float, float>, config::maxSamples> sampleLookupTable = Sampler::populateSampleTable<0>();
 
 template<class Tscene>
 class rayMarcher {
@@ -23,21 +21,12 @@ class rayMarcher {
 private:
 
 
-
-
-
-
-  static float signedSceneDistance(vector point) {
-    return Tscene::signedSceneDistance(point);
-  }
-
-
   static vector getNormal(vector point) {
-    float distanceToObject = signedSceneDistance(point);
+    float distanceToObject = Tscene::signedSceneDistance(point);
 
-    vector normal(signedSceneDistance(point.nudgeX()),
-                  signedSceneDistance(point.nudgeY()),
-                  signedSceneDistance(point.nudgeZ()));
+    vector normal(Tscene::signedSceneDistance(point.nudgeX()),
+                  Tscene::signedSceneDistance(point.nudgeY()),
+                  Tscene::signedSceneDistance(point.nudgeZ()));
 
     return (normal - distanceToObject).normalize();
   }
@@ -48,7 +37,7 @@ private:
     vector currentPoint = origin;
 
     for (int step=0; step < config::traceMaxSteps; step++) {
-      float distanceToObject  =  signedSceneDistance(currentPoint);
+      float distanceToObject  =  Tscene::signedSceneDistance(currentPoint);
       distanceTotal          += distanceToObject;
       currentPoint            = origin + direction * distanceTotal;
 
@@ -60,18 +49,16 @@ private:
 
 
   static color shadePixel(vector origin, vector direction) {
-    vector lightPosition(-2, 6, 4);
-
     auto [_, hitpoint] = sphereTracing(origin, direction);
 
     // calculate diffuse color
-    vector lightDirection = (lightPosition - hitpoint).normalize();
+    vector lightDirection = (Tscene::lightPosition - hitpoint).normalize();
     vector normal         = getNormal(hitpoint);
     float  diffuse        = helper::clamp(normal.dotProduct(lightDirection), 0.0f, 1.0f);
 
     // calculate shadow
     auto [lightDistance, __]   = sphereTracing(hitpoint + normal * 0.1f, lightDirection);
-    if (lightDistance < (lightPosition - hitpoint).length()) diffuse *= 0.25f;
+    if (lightDistance < (Tscene::lightPosition - hitpoint).length()) diffuse *= 0.25f;
 
     return color(diffuse);
   }
@@ -80,18 +67,18 @@ private:
 public:
 
   static void renderLine(int y) {
-    vector cameraOrigin(4, 3, 5);
-
     float stepX = 1.0f / config::width;
     float stepY = 1.0f / config::height;
 
     for (int x = 0; x < config::width; x++) {
       color color;
       for (int sample = 0; sample < config::maxSamples; sample++) {
-        color += shadePixel(cameraOrigin, vector(
-            (config::width/2  - x + std::get<0>(sampler::lookupTable[sample])) * stepX - 0.8f,
-            (config::height/2 - y + std::get<1>(sampler::lookupTable[sample])) * stepY - 0.7f,
-            -1.0f).normalize());
+        vector direction(
+            (config::width/2  - x + std::get<0>(sampler::lookupTable[sample])) * stepX + Tscene::cameraDirection.x,
+            (config::height/2 - y + std::get<1>(sampler::lookupTable[sample])) * stepY + Tscene::cameraDirection.y,
+            Tscene::cameraDirection.z);
+
+        color += shadePixel(Tscene::cameraOrigin, direction.normalize());
 
         outputSdl::pixels[(config::width * y) + x] = color.toNormalizedARGB888();
 
