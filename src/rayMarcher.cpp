383ad3135f67de5e719b842524e519170ba0cc76
Anton Krug <anton.krug@gmail.com>
2021-12-29 02:28:26 +0000

    Moving mhcp distance calculation into mhcp scene

--- src/rayMarcher.cpp
+++ src/rayMarcher.cpp
@@ -4,34 +4,20 @@
 //
 
 #include <limits>
+
 #include "rayMarcher.h"
 
+#include "scenes/mhcp.h"
+
+#include "helper.h"
 #include "outputSdl.h"
 #include "sampler.h"
-#include "helper.h"
-#include "signedDistance.h"
 
 
 const auto sampleLookupTable = Sampler::populateSampleTable<0>();
 
-
 float rayMarcher::signedSceneDistance(vector currentPoint) {
-  float plane  = currentPoint.y + 3.0f;
-
-  float logoCylinder = signedDistance::mhcpLogoCylinder(currentPoint);
-  float logoDistance = logoCylinder;
-
-  if (logoCylinder < 0.05f) {
-    // Use the logoCylinder as boundary box and only calculate the rest of the cylinder details when the ray gets closer
-    float logoPlane = (currentPoint - vector(-1.9f, 0.0f, 0.0f)).dotProduct(vector(-1.0f, 0.637f, 0.0f).normalize());
-    float cut1 = signedDistance::mhcpDodlyDood(
-        currentPoint + vector(0.1f,0.0f,0.0f),
-        vector(0.35f, 0.95f, 2.0f));
-
-    logoDistance = helper::maxf(logoCylinder, logoPlane, -cut1);
-  }
-
-  return helper::minf(plane, logoDistance);
+  return mhcp::signedSceneDistance(currentPoint);
 }
 
 
