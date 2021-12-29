0e17c1e67bbe90621a5a3c4e8e1f0ab6541dea35
Anton Krug <anton.krug@gmail.com>
2021-12-29 02:19:32 +0000

    Dedicated scenes

---
+++ src/scenes/mhcp.cpp
@@ -0,0 +1,8 @@
+//
+// Created by anton.krug@gmail.com on 28/12/21.
+// License: MIT
+//
+
+#include "mhcp.h"
+
+
---
+++ src/scenes/mhcp.h
@@ -0,0 +1,21 @@
+//
+// Created by Fredy on 28/12/21.
+//
+
+#ifndef RAYMARCHER_MHCP_H
+#define RAYMARCHER_MHCP_H
+
+#include "../vector.h"
+
+
+struct mhcp {
+  inline static vector cameraOrigin    = vector(4.0f, 3.0f, 5.0f);
+  inline static vector cameraDirection = vector(- 0.8f, - 0.7f, -1.0f);
+
+  inline static vector lightPosition   = vector(-2.0f, 6.0f, 4.0f);
+
+  static float signedSceneDistance(vector currentPoint);
+};
+
+
+#endif //RAYMARCHER_MHCP_H
