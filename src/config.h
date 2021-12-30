6dfc9081d371be01a3716d69df6b963a1c13c2f7
Anton Krug <anton.krug@gmail.com>
2021-12-30 12:33:08 +0000

    Speed up renders for experiments

--- src/config.h
+++ src/config.h
@@ -8,18 +8,18 @@
 
 namespace config {
   // Render resolution
-  constexpr int width  = 256*2;
-  constexpr int height = 240*2;
+  constexpr int width  = 256*1;
+  constexpr int height = 256*1;
 
   // Display resolution can bigger than render resolution (scaling the image)
-  constexpr int displayWidth  = width  * 2;
-  constexpr int displayHeight = height * 2;
+  constexpr int displayWidth  = width  * 4;
+  constexpr int displayHeight = height * 4;
 
   // Normal calculation offset
   constexpr float nudgeOffset = 0.02f;
 
   // Randomized sampler config values
-  constexpr int maxSamples = 3;
+  constexpr int maxSamples = 1;
 
   // Output config SDL, Mustein, UART
 
