63418ad31aa22505227a1059532b3b589b144988
Anton Krug <anton.krug@gmail.com>
2022-01-01 18:13:27 +0000

    Much better camera angle, roll + perspective

--- src/scenes/mhcp.h
+++ src/scenes/mhcp.h
@@ -11,10 +11,10 @@
 
 
 struct mhcp {
-  inline static vector cameraOrigin    = vector(4.0f, 1.5f, 5.0f);
-  inline static vector cameraDirection = vector(-4.0f, -2.5f, -5.0f).normalize();
+  inline static vector cameraOrigin    = vector(3.0f, 1.5f, 4.5f);
+  inline static vector cameraDirection = vector(-2.2f, -2.1f, -4.5f).normalize();
 
-  inline static vector cameraRightPixel = vector(cameraDirection.z, cameraDirection.y, -cameraDirection.x).normalize() / config::width;
+  inline static vector cameraRightPixel = vector(cameraDirection.z, +0.25f, -cameraDirection.x).normalize() / config::width;
   inline static vector cameraDownPixel  = cameraRightPixel.crossProduct(cameraDirection);
 
   inline static vector lightPosition   = vector(-2.0f, 6.0f, 4.0f);
