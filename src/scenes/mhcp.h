4b6d6e30a4342d77609086e9b31af78ae96ecfe3
Anton Krug <anton.krug@gmail.com>
2022-01-01 18:13:48 +0000

    Styling

--- src/scenes/mhcp.h
+++ src/scenes/mhcp.h
@@ -11,13 +11,13 @@
 
 
 struct mhcp {
-  inline static vector cameraOrigin    = vector(3.0f, 1.5f, 4.5f);
-  inline static vector cameraDirection = vector(-2.2f, -2.1f, -4.5f).normalize();
+  inline static vector cameraOrigin     = vector(3.0f, 1.5f, 4.5f);
+  inline static vector cameraDirection  = vector(-2.2f, -2.1f, -4.5f).normalize();
 
   inline static vector cameraRightPixel = vector(cameraDirection.z, +0.25f, -cameraDirection.x).normalize() / config::width;
   inline static vector cameraDownPixel  = cameraRightPixel.crossProduct(cameraDirection);
 
-  inline static vector lightPosition   = vector(-2.0f, 6.0f, 4.0f);
+  inline static vector lightPosition    = vector(-2.0f, 6.0f, 4.0f);
 
   static float signedDistance(vector point);
 };
