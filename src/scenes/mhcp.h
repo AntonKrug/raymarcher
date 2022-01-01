6d2f921bb5a1687b7681adf1f31ffefb06fb2085
Anton Krug <anton.krug@gmail.com>
2022-01-01 17:56:30 +0000

    Calculate camere right and down vectors scaled for pixels

--- src/scenes/mhcp.h
+++ src/scenes/mhcp.h
@@ -7,11 +7,15 @@
 #define RAYMARCHER_MHCP_H
 
 #include "../vector.h"
+#include "../config.h"
 
 
 struct mhcp {
-  inline static vector cameraOrigin    = vector(7.0f, 4.0f, 7.0f);
-  inline static vector cameraDirection = vector(-1.1f, -0.6f, -1.0f);
+  inline static vector cameraOrigin    = vector(4.0f, 1.5f, 5.0f);
+  inline static vector cameraDirection = vector(-4.0f, -2.5f, -5.0f).normalize();
+
+  inline static vector cameraRightPixel = vector(cameraDirection.z, cameraDirection.y, -cameraDirection.x).normalize() / config::width;
+  inline static vector cameraDownPixel  = cameraRightPixel.crossProduct(cameraDirection);
 
   inline static vector lightPosition   = vector(-2.0f, 6.0f, 4.0f);
 
