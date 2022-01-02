1c5dafc07fe1caccdcc7f03adde4bef135972953
Anton Krug <anton.krug@gmail.com>
2022-01-02 15:45:56 +0000

    Added few materials

---
+++ src/materialE.h
@@ -0,0 +1,17 @@
+//
+// Created by anton.krug@gmail.com on 02/01/22.
+// License: MIT
+//
+
+#ifndef RAYMARCHER_MATERIALE_H
+#define RAYMARCHER_MATERIALE_H
+
+enum class materialE {
+  sky,
+  ground,
+  wall,
+  objectRed,
+  objectLetter
+};
+
+#endif //RAYMARCHER_MATERIALE_H
